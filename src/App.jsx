62641b162507024301f0c321525d9c310a8ef702
anshadOfficial <mkanshadofficial@gmail.com>
2023-12-11 11:45:47 +0530

    updates 90% completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,44 @@ import PatientInvoice from "./components/DashBoard/PatientInvoice";
 import PurchaseInvoice from "./components/DashBoard/PurchaseInvoice";
 import ListofOptions from "./components/DashBoard/ListofOptions";
 
-
 function App() {
   return (
     <>
       <Routes>
-        
         <Route path="/login" element={<InitialLogin />} />
         <Route path="/user-register" element={<UserRegister />} />
         <Route path="/user-login" element={<UserLogin />} />
         <Route path="/signup" element={<InitialSignUp />} />
         {/* <Route path="*" element={<Navigate to={"/"} />} /> */}
-        <Route path="/" element={<HomePage />} >
-          <Route  index element={<PatientInvoice/>}/>
-          <Route  path="patient-invoice" element={<PatientInvoice/>}/>
-          <Route  path="add-option" element={<AddOptions/>}/>
-          <Route  path="purchase-invoice" element={<PurchaseInvoice/>}/>
-          <Route  path="Tables" element={<ListofOptions/>}/>
-          <Route  path="List-invoice" element={<PatientInvoice/>}/>
-          <Route  path="accounts" element={<PatientInvoice/>}/>
-          <Route  path="help" element={<PatientInvoice/>}/>
+        <Route path="/" element={<HomePage />}>
+          <Route
+            index
+            element={
+              <div className="text-2xl uppercase text-center p-10 font-Poppins font-bold">
+                Home page{" "}
+              </div>
+            }
+          />
+          <Route index element={<PatientInvoice />} />
+          <Route path="patient-invoice" element={<PatientInvoice />} />
+          <Route path="add-option" element={<AddOptions />} />
+          <Route path="purchase-invoice" element={<PurchaseInvoice />} />
+          <Route path="Tables" element={<ListofOptions />} />
+          <Route
+            path="List-invoice"
+            element={
+              <div className="text-2xl uppercase text-center p-10 font-Poppins font-bold">
+                List of Invoice{" "}
+              </div>
+            }
+          />
+          <Route path="accounts" element={<div className="text-2xl uppercase text-center p-10 font-Poppins font-bold">
+                Accounts{" "}
+              </div>} />
+          <Route path="help" element={<div className="text-2xl uppercase text-center p-10 font-Poppins font-bold">
+                Support{" "}
+              </div>} />
         </Route>
-        
       </Routes>
     </>
   );
--- src/components/DashBoard/PatientInvoice.jsx
+++ src/components/DashBoard/PatientInvoice.jsx
@@ -2,8 +2,8 @@ import InvoiceForm from "./invoiceForm/PatientInvoiceForm";
 
 const PatientInvoice = () => {
   return (
-    <div className="m-auto w-auto">
-      <div className="topbar m-5 p-4 bg-white flex  flex-wrap justify-center">
+    <div className="m-auto w-auto h-screen ">
+      <div className="topbar m-5 p-4  bg-white  flex  flex-wrap justify-center">
         <h2 className="text-xl font-Inter font-bold uppercase tracking-normal">Patient Invoice</h2>
         <InvoiceForm />
       </div>
